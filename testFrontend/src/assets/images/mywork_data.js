248c3f3d1cf9fe19404862fbe073067af01d2b5c
chiefgit <kameyaw14@gmail.com>
2024-12-20 11:09:27 +0000

    new project

--- testFrontend/src/assets/images/mywork_data.js
+++ testFrontend/src/assets/images/mywork_data.js
@@ -1,46 +1,64 @@
-import project1_img from './project_1.svg'
-import project2_img from './project_2.svg'
-import project3_img from './project_3.svg'
-import project4_img from './project_4.svg'
-import project5_img from './project_5.svg'
-import project6_img from './project_6.svg'
-import docBooking from './docBooking.png'
-import docBooking2 from './docBooking2.png'
-import docBooking3 from './docBooking3.png'
-import docBooking4 from './docBooking4.png'
-import docBooking5 from './docBooking5.png'
-import docBooking6 from './docBooking6.png'
-import ralphrun from './ralphrun.png'
-import ralphrun2 from './ralphrun2.png'
-import ralphrun3 from './ralphrun3.png'
-import ralphrun4 from './ralphrun4.png'
+import project1_img from "./project_1.svg";
+import project2_img from "./project_2.svg";
+import project3_img from "./project_3.svg";
+import project4_img from "./project_4.svg";
+import project5_img from "./project_5.svg";
+import project6_img from "./project_6.svg";
+import docBooking from "./docBooking.png";
+import docBooking2 from "./docBooking2.png";
+import docBooking3 from "./docBooking3.png";
+import docBooking4 from "./docBooking4.png";
+import docBooking5 from "./docBooking5.png";
+import docBooking6 from "./docBooking6.png";
+import ralphrun from "./ralphrun.png";
+import ralphrun2 from "./ralphrun2.png";
+import ralphrun3 from "./ralphrun3.png";
+import ralphrun4 from "./ralphrun4.png";
+import flexcargo1 from "./flexcargo1.png";
+import flexcargo2 from "./flexcargo2.png";
+import flexcargo3 from "./flexcargo3.png";
+import flexcargo4 from "./flexcargo4.png";
+import flexcargo5 from "./flexcargo5.png";
 
 const mywork_data = [
-    {
-        w_no: 1,
-        w_name: "Doctor Booking Site",
-        w_img: docBooking,
-        w_img2: docBooking2,
-        w_img3: docBooking3,
-        w_img4: docBooking4,
-        w_img5: docBooking5,
-        w_img6: docBooking6,
-        website_url: "https://doctor-booking-site-4-frontend.onrender.com/",
-        description: "A comprehensive doctor booking site that allows users to book appointments with doctors, view available time slots, and get reminders.",
-        tech_stack: "React, Node.js, MongoDB"
-    },
-    
-    {
-        w_no: 2,
-        w_name: "Inferno Escape",
-        w_img: ralphrun,
-        w_img2: ralphrun2,
-        w_img3: ralphrun3,
-        w_img4: ralphrun4,
-        website_url: "https://kameyaw14.itch.io/run-ralph",
-        description: "A 2D endless runner game.",
-        tech_stack: "Unity,C#,Stick Nodes Pro Animator"
-    }
+  {
+    w_no: 1,
+    w_name: "Doctor Booking Site",
+    w_img: docBooking,
+    w_img2: docBooking2,
+    w_img3: docBooking3,
+    w_img4: docBooking4,
+    w_img5: docBooking5,
+    w_img6: docBooking6,
+    website_url: "https://doctor-booking-site-4-frontend.onrender.com/",
+    description:
+      "A comprehensive doctor booking site that allows users to book appointments with doctors, view available time slots, and get reminders.",
+    tech_stack: "React, Node.js, MongoDB",
+  },
+
+  {
+    w_no: 2,
+    w_name: "Inferno Escape",
+    w_img: ralphrun,
+    w_img2: ralphrun2,
+    w_img3: ralphrun3,
+    w_img4: ralphrun4,
+    website_url: "https://kameyaw14.itch.io/run-ralph",
+    description: "A 2D endless runner game.",
+    tech_stack: "Unity,C#,Stick Nodes Pro Animator",
+  },
+  {
+    w_no: 3,
+    w_name: "Flex Cargo Site",
+    w_img: flexcargo1,
+    w_img2: flexcargo2,
+    w_img3: flexcargo3,
+    w_img4: flexcargo4,
+    w_img4: flexcargo5,
+    website_url: "https://flex-cargo.onrender.com",
+    description: "An e-commerce clothing site ",
+    tech_stack: "ReactJs, MongoDB,TailwindCSS,NodeJS,ExpressJS,",
+  },
 ];
 
 export default mywork_data;
